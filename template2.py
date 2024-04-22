e8e6c1f752ce6136fb97051622683cf6325bb3c6
crazygeekcolin <115450713+crazygeekcolin@users.noreply.github.com>
2024-04-22 17:31:53 +0800

    0422

---
+++ template2.py
@@ -0,0 +1,39 @@
+textTemplate = """Hi Sir/Madam,
+ 
+We are in peptide business since 2016. We can ship to the UK, EU and the USA.
+Here is the drop shipping price and the USA stock list, if you need please let me know. 
+ 
+
+Item  Specification 
+BPC157 5mg/vial    $ 4.50
+CJC-1295 (DAC)  2mg/vial $ 10.00
+CJC-1295 (with out DAC) 2mg/vial $  4.50
+GHRP-2  5mg/vial $ 3.50
+GHRP-6  5mg/vial $ 3.50
+HCG 5000iu/vial $ 9.00
+HGH (Pharm grade) 10iu/vial $ 10.00
+HGH Frag (176-191) 2mg/vial $ 5.00
+HGH Frag (176-191) 5mg/vial $ 10.00
+HGH (GMP grade) 10iu/vial $ 8.00
+IGF-1LR3 1mg/vial $ 30.00
+MT-II (Melanotan II ) 10mg/vial $ 7.00
+PT-141  (Bremelanotide)  10mg/vial $ 9.00
+Thymosin Beta 4 (TB500) 2mg/vial $ 5.00
+Thymosin Beta 4 (TB500) 5mg/vial $ 10.00
+Tirzepaptide 5mg/vial $ 25.00
+Tirzepaptide 10mg/vial $ 50.00
+Semaglutide 5mg/vial $ 18.00
+IGF-1LR3 1mg/vial $ 25.00
+ 
+Now we ship to the USA directly from China. Small package, high pass rate for the strict US customs. 6 days delivery.
+To EU we can ship through Germany, which is fast and secure.
+ 
+Have a nice day.
+ 
+_________________________
+Kindly regards,
+Colin
+Telegram: Colin1010
+Hong Kong online-peptides Co.,Ltd
+
+ """
