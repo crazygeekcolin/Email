c42eb2658d0fed4bf86547d9597cfd308fa9a8e8
crazygeekcolin <115450713+crazygeekcolin@users.noreply.github.com>
2024-05-09 11:00:47 +0800

    0509

---
+++ template3.py
@@ -0,0 +1,14 @@
+textTemplate_2 = """Hi,
+We provide pharm-grade, generic, finished vials of peptides and HGH, and raw powder of SARM and AAS from the USA and China.
+We have run this business for 7 years, we are proud of the ways of shipping overseas.
+Some hot products include HGH, HCG, Liraglutide, Semaglutide, Anavar and primo E in the USA.
+If you are interested in our service, we can start with a small order. We provide drop shipping services.
+Payment accepted: Bank Transfer /Bitcoin/Zelle/Payoneer
+Have a good day.
+_________________________
+Kindly regards,
+Colin
+Telegram: Colin1010
+Hong Kong online-peptides Co.,Ltd
+
+ """
