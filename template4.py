2be6628bf4362fdfeb0dc60f1303da427156608e
crazygeekcolin <115450713+crazygeekcolin@users.noreply.github.com>
2024-06-22 09:08:57 +0800

    0622

--- template4.py
+++ template4.py
@@ -1,34 +1,25 @@
 textTemplate_4 = """Hi Sir/Madam,
- 
-Now our USA warehouse shipping is back, some peptide in the USA warehouse. Welcome to inquiry.
-If you have new orders, we may ship in advance for you.
 
-We are in peptide business since 2016. We can ship to the UK, EU and the USA.
-Here is the drop shipping price and the USA stock list, if you need please let me know. 
- 
+Our USA warehouse shipping is back, some hot peptides available in the USA warehouse.
 
-Item    Specification    Price
-BPC157 5mg/vial    $ 4.50
-CJC-1295 (DAC)  2mg/vial $ 10.00
-CJC-1295 (with out DAC) 2mg/vial $  4.50
-GHRP-2  5mg/vial $ 3.50
-GHRP-6  5mg/vial $ 3.50
-HCG 5000iu/vial $ 9.00
-HGH (Pharm grade) 10iu/vial $ 10.00
-HGH Frag (176-191) 2mg/vial $ 5.00
-HGH Frag (176-191) 5mg/vial $ 10.00
-HGH (GMP grade) 10iu/vial $ 8.00
-IGF-1LR3 1mg/vial $ 30.00
-MT-II (Melanotan II ) 10mg/vial $ 7.00
-PT-141  (Bremelanotide)  10mg/vial $ 9.00
-Thymosin Beta 4 (TB500) 2mg/vial $ 5.00
-Thymosin Beta 4 (TB500) 5mg/vial $ 10.00
-Tirzepaptide 5mg/vial $ 25.00
-Tirzepaptide 10mg/vial $ 50.00
-Semaglutide 5mg/vial $ 18.00
-IGF-1LR3 1mg/vial $ 25.00
+We are in peptide business since 2016. We have customers in the UK, USA, EU and AU etc.
+We can provide the drop shipping service in the USA, if interested please let me know you are interested in drop shipping or bulk service.
+
+Here is our USA warehouse 1 stock, welcome to inquiry
+
+USA New York stock (June-20-2024)		
+Item	Specification	Quantity
+HCG	5000iu	3000
+HGH Pharm(99%)	10iu	500
+HGH Pharm(99%)	10iu	300
+BPC-157	5mg	500
+TB-500	5mg	700
+Ipamorelin	2mg	440
+Trizepatide	10mg	250
+Trizepatide	5mg	12
+
  
-Now we ship to the USA directly from China. Small package, high pass rate for the strict US customs. 6 days delivery.
+We can also ship to the USA directly from China in small package, we will reship the package if it is in the customs.
 To EU we can ship through Germany, which is fast and secure.
  
 Have a nice day.
@@ -40,3 +31,8 @@ Telegram: Colin1010
 Hong Kong online-peptides Co.,Ltd
 
  """
+
+
+
+
+
